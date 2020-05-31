7d6dd0618b7a50cbcc7d82ab82543240623f522e
IDragnev <iddragnev@gmail.com>
2020-05-31 22:15:40 +0300

    Add a minimal 64-bit kernel for x86

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,20 @@
 
 use core::panic::PanicInfo;
 
+static MESSAGE: &[u8] = b"Rust rocks!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    let colour = 0xb;
+    
+    for (i, &byte) in MESSAGE.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = colour;
+        }
+    }
+
     loop {}
 }
 
