6519dbb824986a289cbb0ee23e5700dabe27db6f
IDragnev <iddragnev@gmail.com>
2020-06-06 23:20:05 +0300

    Implement a driver for the VGA text buffer

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static MESSAGE: &[u8] = b"Rust rocks!";
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    let colour = 0xb;
-    
-    for (i, &byte) in MESSAGE.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = colour;
-        }
-    }
-
+    println!("Welcome to myos!");
     loop {}
 }
 
 #[panic_handler]
-fn panic(_: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
